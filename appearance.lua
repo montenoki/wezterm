327aaedccc55c53885df6b89f2bc29aed8f41013
monten.oki <monten.oki@gmail.com>
2024-04-06 22:07:21 +0900

    update appearance

--- appearance.lua
+++ appearance.lua
@@ -1,16 +1,14 @@
 local module = {}
 
 function module.apply_to_config(config)
-  config.initial_cols = 130
-  config.initial_rows = 40
+  config.color_scheme = 'Afterglow'
+  config.initial_cols = 144
+  config.initial_rows = 50
 
   config.hide_tab_bar_if_only_one_tab = true
   config.use_fancy_tab_bar = false
 
   config.window_decorations = 'RESIZE'
-
-  config.window_background_opacity = 0.7
-  config.macos_window_background_blur = 20
 end
 
 return module
