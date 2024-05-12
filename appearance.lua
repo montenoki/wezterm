57011d884cbb4cc754bdc09d8e4a981b5d419208
monten.oki <monten.oki@gmail.com>
2024-05-13 01:34:48 +0900

    update windows setting

--- appearance.lua
+++ appearance.lua
@@ -4,10 +4,6 @@ function module.apply_to_config(config)
   config.color_scheme = 'Afterglow'
   config.initial_cols = 140
   config.initial_rows = 40
-
-  config.hide_tab_bar_if_only_one_tab = true
-  config.use_fancy_tab_bar = false
-
   config.window_decorations = 'RESIZE'
 end
 
